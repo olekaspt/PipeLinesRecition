6563a3d9f12e3043a748e3d9f7715c6a0775258e
huekylieu <60559196+huekylieu@users.noreply.github.com>
2024-04-15 21:07:15 -0400

    Update UnitTest1.cpp
    Added semi-colon to first test method, should cause compile error

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -14,7 +14,7 @@ namespace UnitTest1
 		{
 
 			CoolClass coolClass;
-			Assert::AreEqual(2, coolClass.Calc(1, 2));
+			Assert::AreEqual(2, coolClass.Calc(1, 2)) ##removed semi-colon, should cause compile error
 		}
 
 		TEST_METHOD(TestMethod2)
